65387e01ba051e54686111ef6d6dc51ef2889d51
Le Tam <letam@Mac-mini-cua-Le.local>
2021-04-24 09:09:37 +0700

    upload

---
+++ src/app/component/user/all-users/all-users.component.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-users',
+  templateUrl: './all-users.component.html',
+  styleUrls: ['./all-users.component.css']
+})
+export class AllUsersComponent implements OnInit {
+
+  token = window.localStorage.getItem('token');
+  data: any;
+  httpJson = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.token,
+      'Access-Control-Allow-Origin': '*',
+    })
+  };
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  getAllUsers(): any {
+    this.http.get<any[]>('http://localhost:3000/user', this.httpJson)
+      .subscribe((data: any) => {
+        this.data = data;
+        console.log('data', data);
+      }, error => {
+        console.log(error);
+      });
+  }
+
+}
